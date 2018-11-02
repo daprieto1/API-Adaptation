b2eae1b234193aecd3b2102bcbb5506adfa888df
daprieto1 <da.prieto1@uniandes.edu.co>
2018-11-02 11:46:36 +0100

    simulate

---
+++ workspaces/broker/broker/src/main/java/edu/uniandes/broker/GreetingController.java
@@ -0,0 +1,28 @@
+package edu.uniandes.broker;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.uniandes.broker.models.Greeting;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+
+    @RequestMapping(value="/temperature",method=RequestMethod.POST)
+    public void saveTemperature() {    	
+        System.out.println("Hi");
+    }
+    
+}
---
+++ workspaces/deviceSimulation/temperatureSensor.js
@@ -0,0 +1,6 @@
+const http = require('http');
+var CronJob = require('cron').CronJob;
+
+new CronJob('* * * * * *', function () {
+    console.log('You will see this message every second');
+}, null, true, 'America/Los_Angeles');
